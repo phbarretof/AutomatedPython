2818e058a133bad2c376fac0d18f328f8791bf1f
phbarretof <105376720+phbarretof@users.noreply.github.com>
2022-12-21 19:38:19 +0000

    adicionei o a.replit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-run = "streamlit run main24.py"
+import streamlit as st
+
+st.title('Primeiro projeto no Streamlit')
+st.subheader('Introduzindo ST no Automated Python')
+st.text('''Meu primeiro texto
+curta ''')
--- main24.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title('Primeiro projeto no Streamlit')
-st.subheader('Introduzindo ST no Automated Python')
-st.text('''Meu primeiro texto
-curta ''')
